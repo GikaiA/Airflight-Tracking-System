c3f952f2e7aa597897225e535fdc1f15697f6ee4
Ant1009th <135274934+Ant1009th@users.noreply.github.com>
2024-04-15 20:07:10 -0400

    working on login page

--- airflight-tracker/src/Login/Login.js
+++ airflight-tracker/src/Login/Login.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './Login.css';
+import airforcewallpaper from '../images/airforcewallpaper.jpg'
+
+const handleLogin = async (e) => {}
 
 function Login() {
   return (
-    <div>
+    <div className='loginpage'>
       
+      <div id = 'loginWrapper'>
+        <form onSubmit={handleLogin}>
+          <label>Username: </label>
+          <input type='text' id='UserNameField'/><br></br>
+          <label>Password: </label>
+          <input type='password' id='PasswordField'/><br></br>
+        </form>
+      </div>
     </div>
   )
 }

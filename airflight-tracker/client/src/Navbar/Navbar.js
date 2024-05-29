1aad006281d36bb8be2ccf6d9ebc660a2696eac7
Gikai Andrews <gnaboys@gmail.com>
2024-05-29 15:00:57 -0400

    uninstalled supabase and deleted code for navbar regarding supabase

--- airflight-tracker/client/src/Navbar/Navbar.js
+++ airflight-tracker/client/src/Navbar/Navbar.js
@@ -2,19 +2,8 @@ import React from "react";
 import "./Navbar.css";
 import { Link, useNavigate } from "react-router-dom";
 import airforcelogononame from "../images/airforcelogo-noname.png";
-import supabase from "../supabaseClient";
 
-function Navbar({ user }) {
-  const naviagate = useNavigate
-  const handleSignOut = async () => {
-    try {
-      await supabase.auth.signOut();
-      naviagate('/')
-    } catch (error) {
-      console.error("Error signing out:", error.message);
-    }
-  };
-
+function Navbar() {
   return (
     <div className="navbar">
       <Link to="/">
@@ -26,24 +15,26 @@ function Navbar({ user }) {
       </Link>
       <ul className="navbar-menu">
         <li className="navbar-item">
-          <Link to='/about' className="navbar-text">About</Link>
+          <Link to="/about" className="navbar-text">
+            About
+          </Link>
         </li>
         <li className="navbar-item">
-          <Link to='/findapilot' className="navbar-text">Find a Pilot</Link>
+          <Link to="/findapilot" className="navbar-text">
+            Find a Pilot
+          </Link>
         </li>
         <li className="navbar-item">
-          <Link to='/search' className="navbar-text">Search</Link>
+          <Link to="/search" className="navbar-text">
+            Search
+          </Link>
         </li>
         <li className="navbar-item">
-          {user ? (
-            <button className="login-button" onClick={handleSignOut}>
-              Sign Out
-            </button>
-          ) : (
-            <button className="login-button">
-              <Link to="/login" className="login-text">Log In</Link>
-            </button>
-          )}
+          <button className="login-button">
+            <Link to="/login" className="login-text">
+              Log In
+            </Link>
+          </button>
         </li>
       </ul>
     </div>

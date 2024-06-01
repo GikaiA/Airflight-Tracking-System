e63c4ed324718ff11af3322c7b0ccdebbf2b147e
Liveaction07 <134444330+Liveaction07@users.noreply.github.com>
2024-06-01 01:05:52 -0400

    edit

--- airflight-tracker/server/routes/authRoutes.js
+++ airflight-tracker/server/routes/authRoutes.js
@@ -5,65 +5,58 @@ const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 
 // User registration route
-// router.post('/register', async (req, res) => {
-//     const { username, email, password, flight_hrs_ttl, night_hrs, nvg_hrs, combat_hrs, combat_sorties, total_sorties, instructor_time, primary_time, secondary_time } = req.body;
-//     try {
-//         const hashedPassword = await bcrypt.hash(password, 10);
-//         const newUser = new User({
-//             username,
-//             email,
-//             password: hashedPassword,
-//             flight_hrs_ttl,
-//             night_hrs,
-//             nvg_hrs,
-//             combat_hrs,
-//             combat_sorties,
-//             total_sorties,
-//             instructor_time,
-//             primary_time,
-//             secondary_time
-//         });
-//         await newUser.save();
-//         res.status(201).send('Registration Successful');
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).send('Registration unsuccessful');
-//     }
-// });
-
 router.post('/register', async (req, res) => {
-    const { email, username, password } = req.body;
-
-    // Check if any of the required fields are missing
-    if (!email || !username || !password) {
-        return res.status(400).send('Missing required fields: email, username, or password');
-    }
-
+    const {username, email, password} = req.body;
     try {
-        // Check for existing user with the same email or username
-        const existingUser = await User.findOne({ $or: [{ email }, { username }] });
-        if (existingUser) {
-            return res.status(409).send('Email or username already exists');
-        }
-
-        // Hash the password
         const hashedPassword = await bcrypt.hash(password, 10);
-
-        // Create a new user
         const newUser = new User({
-            email,
             username,
-            password: hashedPassword
+            email,
+            password: hashedPassword,
         });
-
-        // Save the new user
         await newUser.save();
-        res.status(201).send('Registration successful');
+        res.status(201).send('Registration Successful');
     } catch (error) {
         console.error(error);
         res.status(500).send('Registration unsuccessful');
     }
 });
+
+
+
+// router.post('/register', async (req, res) => {
+//     const { email, username, password } = req.body;
+
+//     // Check if any of the required fields are missing
+//     if (!email || !username || !password) {
+//         return res.status(400).send('Missing required fields: email, username, or password');
+//     }
+
+//     try {
+//         // Check for existing user with the same email or username
+//         const existingUser = await User.findOne({ $or: [{ email }, { username }] });
+//         if (existingUser) {
+//             return res.status(409).send('Email or username already exists');
+//         }
+
+//         // Hash the password
+//         const hashedPassword = await bcrypt.hash(password, 10);
+
+//         // Create a new user
+//         const newUser = new User({
+//             email,
+//             username,
+//             password: hashedPassword
+//         });
+
+//         // Save the new user
+//         await newUser.save();
+//         res.status(201).send('Registration successful');
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).send('Registration unsuccessful');
+//     }
+// });
 
 // User login route
 router.post('/login', async (req, res) => {

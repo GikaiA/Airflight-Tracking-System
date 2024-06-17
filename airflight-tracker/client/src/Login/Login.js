753a2aed82acb87876901940d16ae06bc0d9f94d
Gikai Andrews <gnaboys@gmail.com>
2024-06-17 18:11:07 -0400

    started login page revamp

--- airflight-tracker/client/src/Login/Login.js
+++ airflight-tracker/client/src/Login/Login.js
@@ -1,13 +1,14 @@
-import React, { useState, useContext } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { login as loginService } from '../services/authService';
-import { AuthContext } from '../Context/AuthContext';
-import './Login.css';
+import React, { useState, useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { login as loginService } from "../services/authService";
+import { AuthContext } from "../Context/AuthContext";
+import airforcebg from "../images/airforcebackground.jpg";
+import "./Login.css";
 
 const Login = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const { login } = useContext(AuthContext);
   const navigate = useNavigate();
 
@@ -16,41 +17,46 @@ const Login = () => {
     try {
       const data = await loginService(username, password);
       login(data.userId);
-      navigate('/dashboard');
+      navigate("/dashboard");
     } catch (err) {
       setError(err.message);
     }
   };
 
   return (
-    <div className='login-section'>
-      <div className='login-sub-section'>
-        <form className='login-form' onSubmit={handleLogin}>
-          <h2>Login</h2>
-          <label className='login-label'>Username</label>
-          <input
-            type='text'
-            placeholder='Username'
-            className='login-field'
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <label className='login-label'>Password</label>
-          <input
-            type='password'
-            placeholder='Password'
-            className='login-field'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <button type='submit' className='login-form-button'>
-            Log In
-          </button>
-          {error && <div style={{ color: 'red', marginTop: '10px' }}>{error}</div>}
-          <p className='forget-login-sentence'>
-            Don't have an account? <Link to='/register'>Sign Up</Link>
-          </p>
-        </form>
+    <div className="login-section">
+      <div className="login-split-section">
+        {/* <img src={airforcebg} className="airforce-bg" alt="airforce-bg"></img> */}
+        <div className="login-sub-section">
+          <form className="login-form" onSubmit={handleLogin}>
+            <h2>Login</h2>
+            <label className="login-label">Username</label>
+            <input
+              type="text"
+              placeholder="Username"
+              className="login-field"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <label className="login-label">Password</label>
+            <input
+              type="password"
+              placeholder="Password"
+              className="login-field"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button type="submit" className="login-form-button">
+              Log In
+            </button>
+            {error && (
+              <div style={{ color: "red", marginTop: "10px" }}>{error}</div>
+            )}
+            <p className="forget-login-sentence">
+              Don't have an account? <Link to="/register">Sign Up</Link>
+            </p>
+          </form>
+        </div>
       </div>
     </div>
   );

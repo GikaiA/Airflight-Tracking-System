f020d6db37922f2357b6932258583fbe9b5f2b6c
Gikai Andrews <gnaboys@gmail.com>
2024-06-17 20:25:11 -0400

    did error checking for password field and added picture for login

--- airflight-tracker/client/src/Login/Login.js
+++ airflight-tracker/client/src/Login/Login.js
@@ -2,18 +2,23 @@ import React, { useState, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { login as loginService } from "../services/authService";
 import { AuthContext } from "../Context/AuthContext";
-import airforcebg from "../images/airforcebackground.jpg";
+import airforcebg from "../images/airforcevertical.jpg";
 import "./Login.css";
 
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
   const { login } = useContext(AuthContext);
   const navigate = useNavigate();
 
   const handleLogin = async (e) => {
     e.preventDefault();
+    if(password.length < 6) {
+      setPasswordError("Password must be at least 6 characters long.")
+      return;
+    }
     try {
       const data = await loginService(username, password);
       login(data.userId);
@@ -23,13 +28,23 @@ const Login = () => {
     }
   };
 
+  const handlePasswordChange = (e) => {
+    const newPassword = e.target.value;
+    setPassword(newPassword);
+    if (newPassword.length < 6) {
+      setPasswordError("Password must be at least 6 characters long.")
+    } else{
+      setPasswordError("");
+    }
+  }
+
   return (
     <div className="login-section">
       <div className="login-split-section">
-        {/* <img src={airforcebg} className="airforce-bg" alt="airforce-bg"></img> */}
+        <img src={airforcebg} className="airforce-bg" alt="airforce-bg"></img>
         <div className="login-sub-section">
           <form className="login-form" onSubmit={handleLogin}>
-            <h2>Login</h2>
+            <h2 className="login-title">Login</h2>
             <label className="login-label">Username</label>
             <input
               type="text"
@@ -46,6 +61,11 @@ const Login = () => {
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             />
+            {passwordError && (
+              <div style={{color: 'red', marginTop: "10px"}}>
+                {passwordError}
+              </div>
+            )}
             <button type="submit" className="login-form-button">
               Log In
             </button>

5d0d09edf74c0abfd1795faa05af44b6b8fa4612
Gikai Andrews <gnaboys@gmail.com>
2024-06-23 23:19:54 -0400

    added error checking username and password for login

--- airflight-tracker/client/src/Login/Login.js
+++ airflight-tracker/client/src/Login/Login.js
@@ -2,21 +2,26 @@ import React, { useState, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { login as loginService } from "../services/authService";
 import { AuthContext } from "../Context/AuthContext";
-import airforcebg from "../images/airforcewallpaper.jpg";
+import airforcebg from "../images/airforcebackground.jpg";
 import "./Login.css";
 
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+  const [usernameError, setUsernameError] = useState("");
   const [passwordError, setPasswordError] = useState("");
   const { login } = useContext(AuthContext);
   const navigate = useNavigate();
 
   const handleLogin = async (e) => {
     e.preventDefault();
-    if(password.length < 6) {
-      setPasswordError("Password must be at least 6 characters long.")
+    if (username.trim() === "") {
+      setUsernameError("Enter a valid username.");
+      return;
+    }
+    if (password.length < 6) {
+      setPasswordError("Password must be at least 6 characters long.");
       return;
     }
     try {
@@ -28,41 +33,56 @@ const Login = () => {
     }
   };
 
+  const handleUsernameChange = (e) => {
+    const newUsername = e.target.value;
+    setUsername(newUsername);
+    if (newUsername.trim() === "") {
+      setUsernameError("Enter a valid username.");
+    } else {
+      setUsernameError("");
+    }
+  };
+
   const handlePasswordChange = (e) => {
     const newPassword = e.target.value;
     setPassword(newPassword);
     if (newPassword.length < 6) {
-      setPasswordError("Password must be at least 6 characters long.")
-    } else{
+      setPasswordError("Password must be at least 6 characters long.");
+    } else {
       setPasswordError("");
     }
-  }
+  };
 
   return (
     <div className="login-section">
       <div className="login-split-section">
-        <img src={airforcebg} className="airforce-bg" alt="airforce-bg"></img>
+        <img src={airforcebg} className="airforce-bg" alt="airforce-bg" />
         <div className="login-sub-section">
           <form className="login-form" onSubmit={handleLogin}>
-            <h2 className="login-title">Login</h2>
+            <h2>Login</h2>
             <label className="login-label">Username</label>
             <input
               type="text"
               placeholder="Username"
               className="login-field"
               value={username}
-              onChange={(e) => setUsername(e.target.value)}
+              onChange={handleUsernameChange}
             />
+            {usernameError && (
+              <div className="error-message">
+                {usernameError}
+              </div>
+            )}
             <label className="login-label">Password</label>
             <input
               type="password"
               placeholder="Password"
               className="login-field"
               value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              onChange={handlePasswordChange}
             />
             {passwordError && (
-              <div style={{color: 'red', marginTop: "10px"}}>
+              <div className="error-message">
                 {passwordError}
               </div>
             )}

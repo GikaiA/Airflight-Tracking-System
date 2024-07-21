9fd24f8d25607c57eb3b06404b6f91a527943da9
Ant1009th <135274934+Ant1009th@users.noreply.github.com>
2024-07-20 23:03:44 -0400

    changed '/login' to 'http://localhost:3000/api/auth/login', it made a difference: line 40 Login.js

--- airflight-tracker/client/src/Login/Login.js
+++ airflight-tracker/client/src/Login/Login.js
@@ -37,7 +37,7 @@ const Login = () => {
     }
 
     try {
-      const response = await fetch('/login', {
+      const response = await fetch('http://localhost:3000/api/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
